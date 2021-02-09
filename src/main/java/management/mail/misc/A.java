400e24d935c398557fb2d17da5cdf957d2a8e912
Rekreatur <Iskander73rus@mail.ru>
2021-02-09 17:47:53 +0400

    Fixed

---
+++ src/main/java/management/mail/misc/A.java
@@ -0,0 +1,9 @@
+package management.mail.misc;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class A extends SpringBootServletInitializer {
+  
+}
